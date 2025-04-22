d75aaf9224f08b3b5c636240532d7c6317394f0b
jasonyu99 <yujunven@hotmail.com>
2025-04-22 18:14:08 +0800

    Use SQLite asynchronously

--- People/People/PersonRepository.cs
+++ People/People/PersonRepository.cs
@@ -10,16 +10,16 @@ public class PersonRepository
     public string StatusMessage { get; set; }
 
     // TODO: Add variable for the SQLite connection
-    private SQLiteConnection conn;
+    private SQLiteAsyncConnection conn;
 
-    private void Init()
+    private async Task Init()
     {
         // TODO: Add code to initialize the repository         
         if(conn != null)
             return;
 
-        conn = new SQLiteConnection(_dbPath);
-        conn.CreateTable<Person>();
+        conn = new SQLiteAsyncConnection(_dbPath);
+        await conn.CreateTableAsync<Person>();
     }
 
     public PersonRepository(string dbPath)
@@ -27,19 +27,19 @@ public class PersonRepository
         _dbPath = dbPath;                        
     }
 
-    public void AddNewPerson(string name)
-    {            
+    public async Task AddNewPersonAsync(string name)
+    {
         int result = 0;
         try
         {
-            Init();
+            await Init();
 
-            // basic validation to ensure a name was entered
+            // basic validation to ensure a name was entered  
             if (string.IsNullOrEmpty(name))
                 throw new Exception("Valid name required");
 
-            // TODO: Insert the new person into the database
-            result = conn.Insert(new Person { Name = name });
+            // Insert the new person into the database  
+            result = await conn.InsertAsync(new Person { Name = name });
 
             StatusMessage = string.Format("{0} record(s) added (Name: {1})", result, name);
         }
@@ -47,16 +47,14 @@ public class PersonRepository
         {
             StatusMessage = string.Format("Failed to add {0}. Error: {1}", name, ex.Message);
         }
-
     }
 
-    public List<Person> GetAllPeople()
+    public async Task<List<Person>> GetAllPeopleAsync()
     {
-        // TODO: Init then retrieve a list of Person objects from the database into a list
         try
         {
-            Init();
-            return conn.Table<Person>().ToList();
+            await Init();
+            return await conn.Table<Person>().ToListAsync();
         }
         catch (Exception ex)
         {
@@ -65,4 +63,20 @@ public class PersonRepository
 
         return new List<Person>();
     }
+
+    //public List<Person> GetAllPeople()
+    //{
+    //    // TODO: Init then retrieve a list of Person objects from the database into a list
+    //    try
+    //    {
+    //        Init();
+    //        return conn.Table<Person>().ToList();
+    //    }
+    //    catch (Exception ex)
+    //    {
+    //        StatusMessage = string.Format("Failed to retrieve data. {0}", ex.Message);
+    //    }
+
+    //    return new List<Person>();
+    //}
 }
